1b7b32b6d7ffaadf914a3b3cac33ec7b6c5e0c96
thoapk1 <kimthoap5@gmail.com>
2023-07-01 10:37:20 -0700

    update

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a6e4a3a83ce3a490aadff5a089c141e9-756272119.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
